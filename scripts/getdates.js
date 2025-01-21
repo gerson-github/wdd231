8c73898a29e0f7e02399c6761170c31632b8ff9d
gerson <gersonmoliver@gmail.com>
2025-01-20 23:54:58 -0800

    implementado filtro dinamico

--- scripts/getdates.js
+++
@@ -1,1 +0,0 @@
-const currYear=(new Date).getFullYear(),lastModified=document.lastModified;document.getElementById("lastModified").textContent+=lastModified,document.getElementById("currentYear").textContent=currYear;
